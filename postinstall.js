9796d3314be3c5404d8bb666c3a4fd63e275629e
Hugh Guiney <hugh@hughguiney.com>
2018-07-25 10:29:40 -0500

    Post-install script that copies all libraries specified in package.json’s _lib out of node_modules/ and into src/lib/

---
+++ postinstall.js
@@ -0,0 +1,12 @@
+const read = require( 'fs' ).readFileSync;
+const { spawnSync } = require( 'child_process' );
+const path = require( 'path' );
+
+const package = JSON.parse( read( './package.json', { "encoding": "UTF-8" } ) );
+
+package._lib.forEach( ( library ) => {
+  let dir = path.basename( library );
+
+  spawnSync( 'rm', [ '-rf', `src/lib/${dir}/` ] );
+  spawnSync( 'cp', [ '-r', `node_modules/${library}/`, `src/lib/${dir}` ] );
+} );

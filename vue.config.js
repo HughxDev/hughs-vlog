7458b60ab2d02d325930682fb60a2348236eb353
Hugh Guiney <hugh@hughguiney.com>
2018-07-27 08:52:18 -0500

    public/index.html → src/index.html

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 module.exports = {
+  chainWebpack: config => {
+    config
+      .plugin( 'html' )
+      .tap( args => {
+        // console.log( 'process.env', process.env );
+        if ( process.env.NODE_ENV === 'development' ) {
+          args[0].template = './src/index.html';
+        }
+        return args;
+      } )
+    ;
+  },
   configureWebpack: {
     // chainWebpack: config => {
     //   config.plugins.delete('html')
@@ -11,22 +23,22 @@ module.exports = {
       }
     },
   },
-  devServer: {
-    // historyApiFallback: true,
-    // noInfo: true,
-    // proxy: {
-    //   "/components": {
-    //     target: "http://localhost:80",
-    //     secure: false
-    //   },
-    //   "/lib": {
-    //     target: "http://localhost:80",
-    //     secure: false
-    //   },
-    //   // "/components": {
-    //   //   target: "http://localhost:80",
-    //   //   secure: false
-    //   // }
-    // }
-  }
+  // devServer: {
+  //   historyApiFallback: true,
+  //   noInfo: true,
+  //   proxy: {
+  //     "/components": {
+  //       target: "http://localhost:80",
+  //       secure: false
+  //     },
+  //     "/lib": {
+  //       target: "http://localhost:80",
+  //       secure: false
+  //     },
+  //     // "/components": {
+  //     //   target: "http://localhost:80",
+  //     //   secure: false
+  //     // }
+  //   }
+  // }
 }

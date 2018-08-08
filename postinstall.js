ed829aeb5760d2c3bef1a4440c4562125da22f11
Hugh Guiney <hugh@hughguiney.com>
2018-08-08 02:14:46 -0500

    Corrected for build

--- postinstall.js
+++ postinstall.js
@@ -7,8 +7,8 @@ const package = JSON.parse( read( './package.json', { "encoding": "UTF-8" } ) );
 package._lib.forEach( ( library ) => {
   let dir = path.basename( library );
 
-  // spawnSync( 'rm', [ '-rf', `src/lib/${dir}/` ] );
-  spawnSync( 'rm', [ '-rf', `public/lib/${dir}/` ] );
-  // spawnSync( 'cp', [ '-r', `node_modules/${library}/`, `src/lib/${dir}` ] );
-  spawnSync( 'cp', [ '-r', `node_modules/${library}/`, `public/lib/${dir}` ] );
+  spawnSync( 'rm', [ '-rf', `src/lib/${dir}/` ] );
+  // spawnSync( 'rm', [ '-rf', `public/lib/${dir}/` ] );
+  spawnSync( 'cp', [ '-r', `node_modules/${library}/`, `src/lib/${dir}` ] );
+  // spawnSync( 'cp', [ '-r', `node_modules/${library}/`, `public/lib/${dir}` ] );
 } );
